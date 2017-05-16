2545bfeece6f145e70fc4a66dd0dd2625877eca1
Amir Livne <alivne@campus.technion.ac.il>
2017-05-16 21:12:33 +0300

    started working on account.h

--- account.h
+++ account.h
@@ -1,12 +1,22 @@
+#include <stdlib.h>
 #include <string>
 
+enum result {PASS_ERR, BALANCE_ERR , SUCCESS};
+
+using namespace std;
 
 class account {
 private:
-	int account_number;
+	int account_id;
 	double balance;
 	string password;
-
-
-
-};
+public:
+	account(int id, string pass, double init_balance) : account_id(id), password(pass), balance(init_balance) {}
+	~account();
+	string getPassword();
+	double deposite(double amount);
+	double withdrawal(double amount);
+	double getBalance();
+	
+	
+}
